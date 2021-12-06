2be53ae5a96c56128bbaf5803012ddf5fdf9567f
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-12-06 19:47:19 +0300

    Updates

---
+++ precache-manifest.2f2f517d691fbbe9a6a69c433ab8f916.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f382a9e006a6e6d0f5439e64afdc7251",
+    "url": "/React-Lorem-Component/index.html"
+  },
+  {
+    "revision": "148e8c50b9129ae50dd2",
+    "url": "/React-Lorem-Component/static/css/main.fb86bce3.chunk.css"
+  },
+  {
+    "revision": "d1ccc1e6d556dbe2d9b7",
+    "url": "/React-Lorem-Component/static/js/2.d8a71053.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Lorem-Component/static/js/2.d8a71053.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "148e8c50b9129ae50dd2",
+    "url": "/React-Lorem-Component/static/js/main.9618c182.chunk.js"
+  },
+  {
+    "revision": "21ab9fe979d4d7a9501e",
+    "url": "/React-Lorem-Component/static/js/runtime-main.35d63d65.js"
+  }
+]);
